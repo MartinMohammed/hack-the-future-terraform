d57a5f48bc30bb649a89d55d116ebae3e3fcd9e2
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-02-06 01:46:27 +0100

    instead of archiving through terraform or using null script use local files generated

--- lambda.tf
+++ lambda.tf
@@ -1,36 +1,18 @@
-# Archive for the Lambda handler
-data "archive_file" "tariff_handler_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/dist/handlers/tariff_handler"
-  output_path = "${path.module}/dist/handlers/tariff_handler.zip"
-
-  depends_on = [null_resource.build_lambdas]
-}
-
 resource "aws_lambda_function" "tariff_handler" {
   function_name = "tariff_handler"
-  filename      = data.archive_file.tariff_handler_zip.output_path
+  # use local file
+  filename = "${path.module}/dist/handlers/tariff_handler.zip"
 
   handler     = "index.handler"
   runtime     = "nodejs18.x"
   memory_size = 1024
   timeout     = 300
 
-  source_code_hash = data.archive_file.tariff_handler_zip.output_base64sha256
+  source_code_hash = filebase64sha256("${path.module}/dist/handlers/tariff_handler.zip")
 
   layers = [
     aws_lambda_layer_version.lambda_utils_layer.arn
   ]
 
   role = aws_iam_role.handler_lambda_exec.arn
-}
-
-resource "null_resource" "build_lambdas" {
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-
-  provisioner "local-exec" {
-    command = "./build-lambdas-and-layers.sh"
-  }
 }
