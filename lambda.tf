94df7f28ad3b5e3c9590af2518ed9fdcf2cae447
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-02-06 02:35:16 +0100

    add 'aws_secretsmanager_secret' 'snowflake_secret' and provide secret name to lambda as env. variable

--- lambda.tf
+++ lambda.tf
@@ -7,6 +7,12 @@ resource "aws_lambda_function" "tariff_handler" {
   runtime     = "nodejs18.x"
   memory_size = 1024
   timeout     = 300
+  environment {
+    variables = {
+      SNOWFLAKE_SECRET_NAME = aws_secretsmanager_secret.snowflake_secret.name
+      AWS_REGION            = var.aws_region
+    }
+  }
 
   source_code_hash = filebase64sha256("${path.module}/dist/handlers/tariff_handler.zip")
 
