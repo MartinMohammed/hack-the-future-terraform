d1da10d0d336869f25605cc4f2fbf7e17d4507b8
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-02-06 10:10:50 +0100

    add 'aws_lambda_permission' 'trigger_tariff_handler_permission'

--- api_gateway.tf
+++ api_gateway.tf
@@ -49,7 +49,7 @@ resource "aws_apigatewayv2_route" "tariffs_handler" {
 }
 
 # allows api gateway to execute to invoke the lambda function
-resource "aws_lambda_permission" "api_gw" {
+resource "aws_lambda_permission" "trigger_tariffs_handler_permission" {
   statement_id  = "AllowExecutionFromHackTheFutureBnetzAPI"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.tariffs_handler.function_name
@@ -71,4 +71,13 @@ resource "aws_apigatewayv2_route" "tariff_handler" {
   # allow all http methods
   route_key = "GET /tariffs/{tariff_id}"
   target    = "integrations/${aws_apigatewayv2_integration.tariff_handler.id}"
+}
+
+# allows api gateway to execute to invoke the lambda function
+resource "aws_lambda_permission" "trigger_tariff_handler_permission" {
+  statement_id  = "AllowExecutionFromHackTheFutureBnetzAPI"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.tariff_handler.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
 }
