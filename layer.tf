9d108ee0476e788cf15b8fdc06012eaef9aaed3c
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-02-06 02:04:30 +0100

    trigger ci / cd

--- layer.tf
+++ layer.tf
@@ -1,8 +1,7 @@
 resource "aws_lambda_layer_version" "lambda_utils_layer" {
   layer_name = "shared_utils"
 
-  filename         = "${path.module}/dist/layers/util-layer.zip"
-  source_code_hash = filebase64sha256("${path.module}/dist/layers/util-layer.zip")
-
+  filename            = "${path.module}/dist/layers/util-layer.zip"
+  source_code_hash    = filebase64sha256("${path.module}/dist/layers/util-layer.zip")
   compatible_runtimes = ["nodejs18.x"]
 }
