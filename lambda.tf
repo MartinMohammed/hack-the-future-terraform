ee73b4c6159e348f89d1309cce794018f169f1fd
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-02-06 01:04:19 +0100

    update path for 'filebase64sha256' in 'aws_lambda_function' 'tariff_handler'

--- lambda.tf
+++ lambda.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "tariff_handler" {
   timeout     = 300
 
   # This will force Terraform to update the Lambda when the zip content changes
-  source_code_hash = filebase64sha256("src/tariff_handler.zip")
+  source_code_hash = filebase64sha256("src/handlers/tariff_handler.zip")
 
   role = aws_iam_role.handler_lambda_exec.arn
 }
