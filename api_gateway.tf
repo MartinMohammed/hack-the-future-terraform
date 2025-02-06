2f15cf448cd4e92eeb6a5cb5da04fb7b605062fe
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-02-06 09:46:38 +0100

    remove duplicate api gw permission to trigger lambdas

--- api_gateway.tf
+++ api_gateway.tf
@@ -42,6 +42,7 @@ resource "aws_apigatewayv2_integration" "tariffs_handler" {
 resource "aws_apigatewayv2_route" "tariffs_handler" {
   api_id = aws_apigatewayv2_api.main.id
   # allow all http methodss
+
   route_key = "GET /tariffs"
 
   target = "integrations/${aws_apigatewayv2_integration.tariffs_handler.id}"
@@ -71,13 +72,4 @@ resource "aws_apigatewayv2_route" "tariff_handler" {
   route_key = "GET /tariff/{tariff_id}"
 
   target = "integrations/${aws_apigatewayv2_integration.tariff_handler.id}"
-}
-
-# allows api gateway to execute to invoke the lambda function
-resource "aws_lambda_permission" "api_gw" {
-  statement_id  = "AllowExecutionFromHackTheFutureBnetzAPI"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.tariff_handler.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
 }
