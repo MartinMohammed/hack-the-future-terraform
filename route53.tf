bbe9893a5701f105201461d25ee5c86e02874b17
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-02-07 01:29:19 +0100

    comment out route53 web app record

--- route53.tf
+++ route53.tf
@@ -3,15 +3,15 @@ resource "aws_route53_zone" "therealfriends_zone" {
   name = "therealfriends.de"
 }
 
-# Create an alias record pointing to the CloudFront distribution using prod subdomain
-resource "aws_route53_record" "web_app_record" {
-  zone_id = aws_route53_zone.therealfriends_zone.zone_id
-  name    = "prod.therealfriends.de"
-  type    = "A"
+# # Create an alias record pointing to the CloudFront distribution using prod subdomain
+# resource "aws_route53_record" "web_app_record" {
+#   zone_id = aws_route53_zone.therealfriends_zone.zone_id
+#   name    = "prod.therealfriends.de"
+#   type    = "A"
 
-  alias {
-    name                   = aws_cloudfront_distribution.web_app_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.web_app_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
+#   alias {
+#     name                   = aws_cloudfront_distribution.web_app_distribution.domain_name
+#     zone_id                = aws_cloudfront_distribution.web_app_distribution.hosted_zone_id
+#     evaluate_target_health = false
+#   }
+# }
