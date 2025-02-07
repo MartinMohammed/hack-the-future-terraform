a22e9986783d4eacfac61f831efcdcc25d7aa840
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-02-07 02:03:18 +0100

    add provider alis for us-east-1

--- acm.tf
+++ acm.tf
@@ -1,6 +1,12 @@
+// Define a separate provider configuration for us-east-1
+provider "aws" {
+  alias  = "us_east"
+  region = "us-east-1"
+}
 
-// Create SSL certificate
+// Create SSL certificate in us-east-1 using the alias provider
 resource "aws_acm_certificate" "ssl_cert" {
+  provider          = aws.us_east
   domain_name       = var.domain
   validation_method = "DNS"
 
